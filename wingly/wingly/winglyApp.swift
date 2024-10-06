d7e010a312f0293cf45fc665255596ea6c9fb0cb
Vladimir Popov <vova_57@bk.ru>
2024-10-06 18:26:57 +0300

    added sample ios project

---
+++ wingly/wingly/winglyApp.swift
@@ -0,0 +1,31 @@
+//
+//  winglyApp.swift
+//  wingly
+//
+//  Created by Владимир Попов on 06.10.2024.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct winglyApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
