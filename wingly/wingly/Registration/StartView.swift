6e67176488c45c695d93eeeabdc1cc14a19ca967
Vladimir Popov <vova_57@bk.ru>
2024-10-12 08:30:26 +0300

    added sign-in by phone number and sms push

--- wingly/wingly/Registration/StartView.swift
+++ wingly/wingly/Registration/StartView.swift
@@ -5,113 +5,118 @@ struct StartView: View {
     @State private var widthView = UIScreen.main.bounds.width
     @State private var heightView = UIScreen.main.bounds.height
     
+    @State private var showRegistration = false
     @State private var showDialog = false
     
     var body: some View {
-        VStack {
-            Image(uiImage: UIImage(named: "Logo_start")!)
-                .resizable()
-                .scaledToFit()
-                .padding(heightView * 0.054 / 5)
-                .frame(width: widthView, height: heightView * 0.054)
-            
-            Spacer()
-            
-            Text("Узнайте, что \nпроисходит в мире \nпрямо сейчас")
-                .font(Font.custom("SFProDisplay-Bold", size: 32))
-                .bold()
-                .padding(.trailing, widthView * 0.15)
-                .frame(width: widthView * 0.91, height: heightView * 0.162)
-            
-            Button {
+        if showRegistration {
+            Login()
+        } else {
+            VStack {
+                Image(uiImage: UIImage(named: "Logo_start")!)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(heightView * 0.054 / 5)
+                    .frame(width: widthView, height: heightView * 0.054)
                 
-            } label: {
-                ZStack{
-                    Rectangle()
-                        .frame(width: widthView * 0.91, height: heightView * 0.066)
-                        .cornerRadius(14)
-                        .foregroundColor(.black)
-                    Text("Войти")
-                        .foregroundColor(.white)
-                        .bold()
-                        .font(Font.custom("SFProDisplay-Bold", size: 16))
-                }
-            }
-            .padding(.top, 100)
-            
-            ZStack {
-                Rectangle()
-                    .frame(width: widthView * 0.91, height: 1)
-                    .foregroundColor(.gray)
-                    .opacity(0.5)
+                Spacer()
                 
-                Text("   или   ")
-                    .foregroundColor(.gray)
-                    .opacity(0.5)
+                Text("Узнайте, что \nпроисходит в мире \nпрямо сейчас")
+                    .font(Font.custom("SFProDisplay-Bold", size: 32))
                     .bold()
-                    .background(.white)
-                    .font(Font.custom("SFProDisplay-Bold", size: 16))
-            }
-            .padding(.top, heightView * 0.02)
-            .padding(.bottom, heightView * 0.02)
-            
-            Button {
+                    .padding(.trailing, widthView * 0.15)
+                    .frame(width: widthView * 0.91, height: heightView * 0.162)
                 
-            } label: {
-                ZStack{
+                Button {
+                    showRegistration = true
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .frame(width: widthView * 0.91, height: heightView * 0.066)
+                            .cornerRadius(14)
+                            .foregroundColor(.black)
+                        Text("Войти")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(Font.custom("SFProDisplay-Bold", size: 16))
+                    }
+                }
+                .padding(.top, 100)
+                
+                ZStack {
                     Rectangle()
-                        .frame(width: widthView * 0.91, height: heightView * 0.066)
-                        .cornerRadius(14)
-                        .foregroundColor(.white)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(.black, lineWidth: 1)
-                        )
-                        
-                    Text("Зарегистрироваться")
-                        .foregroundColor(.black)
+                        .frame(width: widthView * 0.91, height: 1)
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
+                    
+                    Text("   или   ")
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
                         .bold()
+                        .background(.white)
                         .font(Font.custom("SFProDisplay-Bold", size: 16))
                 }
-            }
-            
-            Spacer()
-            
-            Button {
-                showDialog = true
-            } label: {
-                VStack(alignment: .leading) {
-                    Text("Регистрируясь, вы принимаете ")
-                        .foregroundColor(Color(hex: "898989"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
-                    + Text("Условия сервиса,")
-                        .foregroundColor(Color(hex: "865DE6"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
-                    Text("Политику конфиденциальности ")
-                        .foregroundColor(Color(hex: "865DE6"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
-                    + Text("и ")
-                        .foregroundColor(Color(hex: "898989"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
-                     + Text("Политику")
-                        .foregroundColor(Color(hex: "865DE6"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
-                    Text("в отношении файлов cookie")
-                        .foregroundColor(Color(hex: "865DE6"))
-                        .font(Font.custom("SFProDisplay-Bold", size: 14))
+                .padding(.top, heightView * 0.02)
+                .padding(.bottom, heightView * 0.02)
+                
+                Button {
+                    showRegistration = true
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .frame(width: widthView * 0.91, height: heightView * 0.066)
+                            .cornerRadius(14)
+                            .foregroundColor(.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        Text("Зарегистрироваться")
+                            .foregroundColor(.black)
+                            .bold()
+                            .font(Font.custom("SFProDisplay-Bold", size: 16))
+                    }
                 }
+                
+                Spacer()
+                
+                Button {
+                    showDialog = true
+                } label: {
+                    VStack(alignment: .leading) {
+                        Text("Регистрируясь, вы принимаете ")
+                            .foregroundColor(Color(hex: "898989"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                        + Text("Условия сервиса,")
+                            .foregroundColor(Color(hex: "865DE6"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                        Text("Политику конфиденциальности ")
+                            .foregroundColor(Color(hex: "865DE6"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                        + Text("и ")
+                            .foregroundColor(Color(hex: "898989"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                        + Text("Политику")
+                            .foregroundColor(Color(hex: "865DE6"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                        Text("в отношении файлов cookie")
+                            .foregroundColor(Color(hex: "865DE6"))
+                            .font(Font.custom("SFProDisplay-Bold", size: 14))
+                    }
+                }
+                .frame(width: widthView * 0.91, height: heightView * 0.07)
             }
-            .frame(width: widthView * 0.91, height: heightView * 0.07)
-        }
-        .confirmationDialog("", isPresented: $showDialog) {
-            Button("Условия сервиса") {
-                showDialog = false
-            }
-            Button("Политика конфиденциальности") {
-                showDialog = false
-            }
-            Button("Политика в отноiyнии файлов cookie") {
-                showDialog = false
+            .confirmationDialog("", isPresented: $showDialog) {
+                Button("Условия сервиса") {
+                    showDialog = false
+                }
+                Button("Политика конфиденциальности") {
+                    showDialog = false
+                }
+                Button("Политика в отноiyнии файлов cookie") {
+                    showDialog = false
+                }
             }
         }
     }
